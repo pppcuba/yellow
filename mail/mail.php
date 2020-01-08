02053054be3e4ea64a653110ea03208a01f19321
Luis <59265935+nemo7812@users.noreply.github.com>
2020-01-08 16:02:38 -0500

    Update mail.php

--- mail/mail.php
+++ mail/mail.php
@@ -19,13 +19,5 @@ Message: ".$message."
 mail ("luisbmartinez78@gmail.com" , ".$subject.", ".$message.", "FROM: ".$name." <".$email.">\n");
 //header("location: mail-success.html ");
 ?>
-<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h2>Su correo ha sido recibido con éxito</h2>
-						<p class="black-test">nos pondremos en contacto con usted en breve a través del correo electrónico</p>
-						<a href="index.html" class="button"><i class="fa fa-arrow-left"></i>Regresar</a>
-					</div>
-				</div>
-			</div>
+				
 
