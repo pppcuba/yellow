b50c158ca0690956c2b9c85f71b8aa256bd81ee8
pppcuba <59216460+pppcuba@users.noreply.github.com>
2020-01-07 21:40:28 -0300

    Add files via upload

---
+++ mail/mail.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST{'name'};
+$email = $_POST{'email'};
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_message = "
+
+Name: ".$name."
+Email: ".$email."
+Subject: ".$subject."
+Message: ".$message."
+
+";
+
+//mail($replyemail,"","","FROM: ".$nameContacto." <".$emailContacto.">\n");
+
+
+mail ("abeponce@gmail.com" , ".$subject.", ".$message.", "FROM: ".$name." <".$email.">\n");
+header("location: ../mail-success.html ");
+?>
+
+
